0c3f664e68f36c0954c46e711435e25fe1400775
daniele-aceti <daniele.aceti@hotmail.it>
2024-12-10 19:23:40 +0100

    fix: correzione else

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 const distance = prompt(`Quanti Km vuoi percorrere?`);
 const age = prompt(`Quanti anni hai?`);
 
-const ticket = 0.21 * distance;
+const ticket = 0.21 * Number(distance);
 
 if(isNaN(distance) || isNaN(age)){
     console.log(`Inserisci un numero valido, ricarica la pagina`)
-}else { 
-    if (age >= 65){
+}else if (age >= 65){
     const sale40 = ticket - (ticket*40)/100;
     console.log(`€${sale40.toFixed(2).replace(".",",")}`)
 }else if (age <= 18){
     const sale20 = ticket - (ticket*20)/100;
     console.log(`€${sale20.toFixed(2).replace(".",",")}`)
-
 }else{
-    console.log(`€${ticket.toFixed(2).replace(".",",")}`)
-    
-}
+    console.log(`€${ticket.toFixed(2).replace(".",",")}`) 
 }
